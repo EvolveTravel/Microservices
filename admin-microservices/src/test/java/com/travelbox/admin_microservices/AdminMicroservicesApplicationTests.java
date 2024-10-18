0f0efabfdce45bc9da13e8208326d7b6ae4caf4a
Alessandro  Vega <83603934+Alessandrovega@users.noreply.github.com>
2024-10-17 19:29:48 -0500

    Update Microservices configuration
    Configuraciones

---
+++ admin-microservices/src/test/java/com/travelbox/admin_microservices/AdminMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelbox.admin_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ef4e90cf34afdcbcbe36869b396ea66cacce8866
Alessandro  Vega <83603934+Alessandrovega@users.noreply.github.com>
2024-11-03 15:41:55 -0500

    Circuit Breaker Implemented

--- api-gateway/src/main/java/com/travelbox/api_gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/travelbox/api_gateway/routes/Routes.java
@@ -9,9 +9,6 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
-import static org.springframework.web.servlet.function.RouterFunctions.route;
-
-
 @Configuration
 public class Routes {
 
@@ -86,51 +83,4 @@ public class Routes {
                 "/tripFallback"
         );
     }
-
-    /*
-    static private RouterFunction<ServerResponse> generalTravelBoxRoute(String routeId, String path, String url){
-        String basePath = "/api/v1/";
-        return GatewayRouterFunctions.route(routeId).
-                route(RequestPredicates.path(basePath + path), HandlerFunctions.http(url)).build();
-    }
-
-
-    @Bean
-    public RouterFunction<ServerResponse> productServiceRoute() {
-
-        return GatewayRouterFunctions.route("product")
-                .route(RequestPredicates.path("/api/v1/products"),
-                        HandlerFunctions.http("http://localhost:8091")).build();
-    }
-
-    @Bean
-    public RouterFunction<ServerResponse> cartServiceRoute() {
-        return GatewayRouterFunctions.route("cart")
-                .route(RequestPredicates.path("/api/v1/carts"),
-                        HandlerFunctions.http("http://localhost:8092")).build();
-    }
-
-    @Bean
-    public RouterFunction<ServerResponse> userServiceRoute() {
-        return GatewayRouterFunctions.route("user")
-                .route(RequestPredicates.path("/api/v1/users"),
-                        HandlerFunctions.http("http://localhost:8093")).build();
-    }
-
-    @Bean
-    public RouterFunction<ServerResponse> orderServiceRoute() {
-        return GatewayRouterFunctions.route("orders")
-                .route(RequestPredicates.path("/api/v1/orders"),
-                        HandlerFunctions.http("http://localhost:8094")).build();
-    }
-
-    @Bean
-    public RouterFunction<ServerResponse> tripServiceRoute() {
-        return GatewayRouterFunctions.route("trip")
-                .route(RequestPredicates.path("/api/v1/trips"),
-                        HandlerFunctions.http("http://localhost:8095")).build();
-    }
-
-
-    */
 }

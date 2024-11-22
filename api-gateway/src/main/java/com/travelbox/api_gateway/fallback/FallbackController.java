f03ed38973b125274391d46fc9ea41953b59a4f4
Alessandro  Vega <83603934+Alessandrovega@users.noreply.github.com>
2024-11-21 21:40:41 -0500

    deployment configuration

--- api-gateway/src/main/java/com/travelbox/api_gateway/fallback/FallbackController.java
+++ api-gateway/src/main/java/com/travelbox/api_gateway/fallback/FallbackController.java
@@ -9,26 +9,39 @@ public class FallbackController {
 
     @RequestMapping("/productFallback")
     public ResponseEntity<String> productFallback() {
-        return ResponseEntity.status(500).body("Product Service is currently unavailable. Please try again later");
+        return ResponseEntity.status(500).body(
+                "Product Service is currently unavailable. Please try again later");
     }
 
     @RequestMapping("/cartFallback")
     public ResponseEntity<String> cartFallback() {
-        return ResponseEntity.status(500).body("Cart Service is currently unavailable. Please try again later");
+        return ResponseEntity.status(500).body(
+                "Cart Service is currently unavailable. Please try again later");
     }
 
     @RequestMapping("/userFallback")
     public ResponseEntity<String> userFallback() {
-        return ResponseEntity.status(500).body("User Service is currently unavailable. Please try again later");
+        return ResponseEntity.status(500).body(
+                "User Service is currently unavailable. Please try again later");
     }
 
     @RequestMapping("/orderFallback")
     public ResponseEntity<String> orderFallback() {
-        return ResponseEntity.status(500).body("Order Service is currently unavailable. Please try again later");
+        return ResponseEntity.status(500).body(
+                "Order Service is currently unavailable. Please try again later");
     }
 
     @RequestMapping("/tripFallback")
     public ResponseEntity<String> tripFallback() {
-        return ResponseEntity.status(500).body("Trip Service is currently unavailable. Please try again later");
+        return ResponseEntity.status(500).body(
+                "Trip Service is currently unavailable. Please try again later");
     }
 }
+
+
+
+
+
+
+
+
